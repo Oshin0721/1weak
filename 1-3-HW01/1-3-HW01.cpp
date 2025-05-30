07c7eef1ed5fa7f6d4b74e6cf38a350622bc1dd9
Oshin0721 <ehdgk3210@naver.com>
2025-05-30 16:52:26 +0900

    commit

---
+++ 1-3-HW01/1-3-HW01.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+class Battery {
+    private:
+        int charge;
+
+    public:
+        Battery() {
+            charge = 100;
+        }
+        
+        int getCharge() const {
+            return charge;
+        }
+
+        void useBattery() {
+            if (charge >= 5) {
+                charge -= 5;
+            }
+            else {
+                charge = 0;
+            }
+            std::cout << "Battery used. charge : " << charge << "%.\n";
+        }
+
+        void chgargeBattery() {
+            if (charge <= 93) {
+                charge += 7;
+            } else {
+                charge = 100;
+            }
+			std::cout << "Battery charged. charge : " << charge << "%.\n";
+        }
+};
+
+
+
+int main()
+{
+    Battery b;
+
+	std::cout << "Initial battery charge: " << b.getCharge() << "%\n";
+
+    b.useBattery();
+    b.useBattery();
+    b.chgargeBattery();
+    b.useBattery();
+}
+
+
+
